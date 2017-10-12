bb60e3800b150d6bc53c5a0c67f881709c4f35f7
SonyEuro <Fenixmarcus2010@gmail.com>
2017-10-12 19:43:35 +0800

    Реализован Автомат. Изменён интерфейс. Реализована работа. Добавлена иконка.

---
+++ AutomatMP.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lab
+{
+
+    /// <summary>
+    /// Автомат с магазинной памятью.
+    /// </summary>
+    class AutomatMP
+    {
+        /// <summary>
+        /// Стек.
+        /// </summary>
+        List<char> stack = new List<char>();
+        ///// <summary>
+        ///// Верхушка стека. //Должна быть ссылка на верхушку стека.
+        ///// </summary>
+        //char stackTop = stack[0];
+        /// <summary>
+        /// Символ конца стека. А так же конца ленты.
+        /// </summary>
+        const char endStack = '&';
+        /// <summary>
+        /// Состояния автомата.
+        /// </summary>
+        enum state { q0, q1, q2 };
+        /// <summary>
+        /// Действия автомата.
+        /// </summary>
+        enum action { push, pop, confirm };
+        /// <summary>
+        /// Правила автомата.
+        /// </summary>
+        object[,] rules;
+        /// <summary>
+        /// Текущее состояние автомата.
+        /// </summary>
+        const int st0 = 0;
+        /// <summary>
+        /// Входной символ в автомат.
+        /// </summary>
+        const int inCh = 1;
+        /// <summary>
+        /// Верхушка стека автомата.
+        /// </summary>
+        const int stckTop = 2;
+        /// <summary>
+        /// Следущее состояние автомата.
+        /// </summary>
+        const int st1 = 3;
+        /// <summary>
+        /// Действие автомата.
+        /// </summary>
+        const int doing = 4;
+        /// <summary>
+        /// Текущее состояние автомата.
+        /// </summary>
+        state st;
+        /// <summary>
+        /// Подверждает принадлежит входная строка языку или нет.
+        /// </summary>
+        public bool confirmation = false;
+        /// <summary>
+        /// Таблица функицй переходов.
+        /// </summary>
+        public List<String[]> JumpFunction = new List<string[]>();
+        /// <summary>
+        /// Магазин в таблице.
+        /// </summary>
+        const int stackS = 0;
+        /// <summary>
+        /// Состояния в таблице.
+        /// </summary>
+        const int stateS = 1;
+        /// <summary>
+        /// Входная строка.
+        /// </summary>
+        const int instr = 2;
+        /// <summary>
+        /// Конструктор.
+        /// Добавляет в стек конце стека. Формирует правила работы. Устанавливает текущее состояние в начальное, то есть в q0.
+        /// </summary>
+        public AutomatMP()
+        {
+            //добавляем символ конца в стек
+            stack.Add(endStack);
+            createRules();
+            st = state.q0;
+        }
+        /// <summary>
+        /// Создание правил работы распознавания слов языка
+        /// </summary>
+        private void createRules()
+        {
+            rules = new object[5, 5] {
+                {state.q0,  '0',        endStack,   state.q1,   action.push},//typeof(AutomatMP).GetMethod("push").MethodHandle.GetFunctionPointer();
+                {state.q1,  '0',        '0',        state.q1,   action.push},
+                {state.q1,  '1',        '0',        state.q2,   action.pop},
+                {state.q2,  '1',        '0',        state.q2,   action.pop},
+                {state.q2,  endStack,   endStack,   state.q0,   action.confirm},
+            };
+        }
+        /// <summary>
+        /// Возвращение к начальному состоянию.
+        /// </summary>
+        private void SetNullState()
+        {
+            JumpFunction.Clear();
+            stack.Clear();
+            stack.Add(endStack);
+            st = state.q0;
+            confirmation = false;
+        }
+        /// <summary>
+        /// Занести в стек.
+        /// </summary>
+        /// <param name="ch"/> Символ заносимый в стек.
+        void push(char ch)
+        {
+            stack.Insert(0, ch);
+            //stack[++stackTop] = ch;
+        }
+        /// <summary>
+        /// Вытолкнуть из стека.
+        /// </summary>
+        void pop()
+        {
+            if (stack[0] != endStack)
+                stack.RemoveAt(0);
+            //ch = stack[stackTop--];
+        }
+        /// <summary>
+        /// Подтверждение что слово принадлежит языку
+        /// </summary>
+        void confirm()
+        {
+            confirmation = true;
+        }
+        /// <summary>
+        /// Запуск автомата с входной строкой.
+        /// </summary>
+        /// <param name="tape"/> Входная строка.
+        public void Run(string tape)
+        {
+            SetNullState();
+            tape += '&';
+            for (int i = 0; i < tape.Length; i++)
+            {
+                JumpFunction.Add(new String[] { new string(stack.ToArray()), st.ToString(), tape.Substring(i,tape.Length-i)});
+                for (int m = 0; m < 5 //rules.Rank
+                    ; m++)
+                {
+                    if ((st == (state)rules[m, st0]) &&
+                        (tape[i] == (char)rules[m, inCh]) &&
+                        (stack[0] == (char)rules[m, stckTop]))
+                    {
+                        st = (state)rules[m, st1];
+                        //(IntPtr)rules[m, doing]('0');
+                        switch (rules[m, doing])
+                        {
+                            case action.push:
+                                push('0');
+                                break;
+                            case action.pop:
+                                pop();
+                                break;
+                            case action.confirm:
+                                confirm();
+                                break;
+                        }
+                        break;
+                    }
+                    if (m == 4)
+                        return;
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,25 +18,41 @@ using System.Windows.Shapes;
 namespace Lab
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml.
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Всплывающая подсказака при ошибке.
+        /// </summary>
+        ToolTip tp = new ToolTip();
+
+        /// <summary>
+        /// Конструктор главной формы.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            //содержание подсказки
             tp.Content = "Разрешенные символы: 0 и 1. Повторите ввод.";
+            //назначаем всплывающую подсказку объекту
             InputText.ToolTip = tp;
+            //задержка отображения
             ToolTipService.SetShowDuration(InputText, 2000);
+            //расположение подсказки
             tp.Placement = System.Windows.Controls.Primitives.PlacementMode.Top;
+            //расположение относительно объекта
             tp.PlacementTarget = InputText;
+            CreateTables();
         }
-        
-        ToolTip tp = new ToolTip();
-        
+
+
+        /// <summary>
+        /// Разрешён ввод только 1 и 0.
+        /// </summary>
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
-          
+
             if ((e.Key == Key.D0) || (e.Key == Key.D1) || (e.Key == Key.NumPad0) || (e.Key == Key.NumPad1))
             {
                 tp.IsOpen = false;
@@ -44,52 +60,174 @@ namespace Lab
             }
             else
             {
-                ((ToolTip)InputText.ToolTip).IsOpen = true;                
+                ((ToolTip)InputText.ToolTip).IsOpen = true;
                 e.Handled = true;
             }
-                
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void InputText_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            DataTable dt = new DataTable();
-            Tape.Columns.Clear();
-            List<String> str = new List<string>();
-            dt.Rows.Add();
-            for (int i=0;i< InputText.Text.Length;i++)
+            //что бы пробел не вводился
+            if (e.Key == Key.Space)
             {
-                dt.Columns.Add();
-                str.Add(InputText.Text[i].ToString());
+                ((ToolTip)InputText.ToolTip).IsOpen = true;
+                e.Handled = true;
             }
-            dt.Rows[0].ItemArray = str.ToArray();
-            Tape.ItemsSource = dt.DefaultView;
-            //Tape.Columns.Add((DataGridColumn)(new DataGridTextColumn()));
+        }
+
+ //       private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+//            DataTable dt = new DataTable();
+//            Tape.Columns.Clear();
+ //           List<String> str = new List<string>();
+//            dt.Rows.Add();
+ //           for (int i = 0; i < InputText.Text.Length; i++)
+ //           {
+ //               dt.Columns.Add();
+ //               str.Add(InputText.Text[i].ToString());
+ //           }
+  //          dt.Rows[0].ItemArray = str.ToArray();
+ //           Tape.ItemsSource = dt.DefaultView;
+ //           //Tape.Columns.Add((DataGridColumn)(new DataGridTextColumn()));
             //((DataGridColumn)Tape.Columns[0]).Background = new SolidColorBrush(Colors.Red);
             //Tape.SelectedItem = 0;
             //Tape.SelectedIndex = 3;
-            
 
 
-            DataTable dt2 = new DataTable();
-            Stack.Columns.Clear();
-            List<String> str2 = new List<string>();
+
+     //       DataTable dt2 = new DataTable();
+     //       Stack.Columns.Clear();
+    //        List<String> str2 = new List<string>();
             //dt2.Rows.Add();
-            dt2.Columns.Add();
+    //        dt2.Columns.Add();
 
-            for (int i = 0; i < InputText.Text.Length; i++)
-            {
-                //dt2.Rows.Add();
-                str2.Add(InputText.Text[i].ToString());
+       //     for (int i = 0; i < InputText.Text.Length; i++)
+      //      {
+     //           //dt2.Rows.Add();
+      //          str2.Add(InputText.Text[i].ToString());
                 //dt2.Columns.Add(new DataColumn());
-                dt2.Rows.Add(new String[]{ str2[i].ToString() });
+     //           dt2.Rows.Add(new String[] { str2[i].ToString() });
                 //dt2.Rows[i].ItemArray[0] = ;
-            }
+       //     }
             //dt2.Rows[0].ItemArray[0] = str2[0];
             //dt2.Rows[0].ItemArray = str2[0].ToArray();
-            Stack.ItemsSource = dt2.DefaultView;
-            Stack.MinColumnWidth = 68;
-            Stack.MaxColumnWidth = 68;
-            Stack.ColumnWidth = 68;
+     //       Stack.ItemsSource = dt2.DefaultView;
+    //        Stack.MinColumnWidth = 68;
+     //       Stack.MaxColumnWidth = 68;
+     //       Stack.ColumnWidth = 68;
+      //  }
+
+        private void InputText_KeyUp(object sender, KeyEventArgs e)
+        {
+            //if (e.Key == Key.Space)
+            //    e.Handled = false;
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dt4.Rows.Clear();
+            //AutomatMP at = new AutomatMP();
+            Permanent(at);
+            JumpFunction = at.JumpFunction;
+            foreach (string[] str in JumpFunction)
+            {
+                dt4.Rows.Add(str);
+            }
+            Success(at);
+        }
+
+        /// <summary>
+        /// Запуск автомата в режиме "непрерывно".
+        /// </summary>
+        private void Permanent(AutomatMP at)
+        {
+            string tape = InputText.Text;
+            at.Run(tape);
+        }
+
+        private void Success(AutomatMP at)
+        {
+            if (at.confirmation)
+            {
+                InputText.Background = Brushes.LightGreen;
+            }
+            else
+            {
+                InputText.Background = Brushes.Red;
+            }
+        }
+
+        List<String[]> JumpFunction;
+        DataTable dt4;
+        void CreateTables()
+        {
+            DataTable dt3 = new DataTable();
+            JumpTb.Columns.Clear();
+            //dt2.Rows.Add();
+            dt3.Columns.Add("Магазин");
+            dt3.Columns.Add("0");
+            dt3.Columns.Add("1");
+            dt3.Columns.Add("&");
+            dt3.Rows.Add(new String[] { "0", "↓0,→", "↑→", "Отвергнуть" });
+            dt3.Rows.Add(new String[] { "&", "↓0,→", "Отвергнуть", "Допустить" });
+
+            JumpTb.ItemsSource = dt3.DefaultView;
+            //JumpTb.MinColumnWidth = 68;
+            //JumpTb.MaxColumnWidth = 68;
+            //JumpTb.ColumnWidth = 68;
+
+            dt4 = new DataTable();
+            JumpFunctionTb.Columns.Clear();
+            //dt2.Rows.Add();
+            dt4.Columns.Add("Магазин");
+            dt4.Columns.Add("Состояние");
+            dt4.Columns.Add("Исходная строка");
+            //dt3.Columns.Add();
+            //dt3.Columns.Add();
+            //dt3.Columns.Add();
+            //dt3.Columns.Add();
+
+            //dt4.Rows.Add(new String[] { "&", "q0", "0011&" });
+            //dt4.Rows.Add(new String[] { "&0", "q1", "011&" });
+
+            JumpFunctionTb.ItemsSource = dt4.DefaultView;
+            //JumpTb.MinColumnWidth = 68;
+            //JumpTb.MaxColumnWidth = 68;
+            //JumpTb.ColumnWidth = 68;
+        }
+        AutomatMP at = new AutomatMP();
+        int k = 0;
+        int q = 0;
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (q == 0)
+            {
+                dt4.Rows.Clear();
+                InputText.Background = Brushes.White;
+                //AutomatMP at = new AutomatMP();
+                Permanent(at);
+                JumpFunction = at.JumpFunction;
+                k = 0;
+                DiscreteBtn.Content = "Далее";
+                InputText.IsReadOnly = true;
+                q = 1;
+            }
+            else
+            {
+                
+                if (k < JumpFunction.Count)
+                    dt4.Rows.Add(JumpFunction[k++]);
+                if (k == JumpFunction.Count)
+                {
+                    DiscreteBtn.Content = "Дискретно";
+                    InputText.IsReadOnly = false;
+                    q = 0;
+                    Success(at);
+                    
+                }
+            }
+        }
+        
+
     }
 }
